a66fa356f69b944b493005be26a92916590f4e60
Ilyas Turki <turki.ilyass@gmail.com>
2026-04-17 10:41:39 +0200

    build(nix): read version from Cargo.toml and re-enable tests

--- flake.nix
+++ flake.nix
@@ -3,72 +3,89 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+    in
     {
-      self,
-      nixpkgs,
-      flake-utils,
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
 
-        nativeBuildInputs = with pkgs; [
-          pkg-config
-        ];
-
-        buildInputs = with pkgs; [
-          openssl
-          dbus
-        ];
-      in
-      {
-        packages = {
           prayer-times = pkgs.rustPlatform.buildRustPackage {
             pname = "prayer-times";
-            version = "0.4.0";
+            version = cargoToml.package.version;
 
             src = pkgs.lib.cleanSource ./.;
             cargoLock.lockFile = ./Cargo.lock;
 
-            inherit nativeBuildInputs buildInputs;
+            nativeBuildInputs = with pkgs; [ pkg-config ];
+            buildInputs = with pkgs; [
+              openssl
+              dbus
+            ];
 
-            # test_cairo_prayer_times_march_15_2024 fails due to a
-            # pre-existing bug: timezone_to_offset uses Utc::now() instead of
-            # the target date, so DST-sensitive assertions drift over time.
-            doCheck = false;
+            # config::tests::test_config_from_str asserts a debug-mode asset
+            # path; it only passes in debug builds.
+            checkFlags = [ "--skip=config::tests::test_config_from_str" ];
 
             postInstall = ''
-              install -Dm644 assets/mosque-svgrepo-com.png $out/share/icons/hicolor/128x128/apps/prayer-times.png
+              install -Dm644 assets/mosque-svgrepo-com.png \
+                $out/share/icons/hicolor/128x128/apps/prayer-times.png
+
+              # generate-shell writes to env!(CARGO_MANIFEST_DIR)/target/completions
+              # which is the build directory at compile time (still writable here).
+              $out/bin/prayer-times generate-shell
+              completions=$(find . -type d -name completions -path '*/target/*' | head -n1)
+              install -Dm644 "$completions/prayer-times.bash" \
+                "$out/share/bash-completion/completions/prayer-times"
+              install -Dm644 "$completions/_prayer-times" \
+                "$out/share/zsh/site-functions/_prayer-times"
+              install -Dm644 "$completions/prayer-times.fish" \
+                "$out/share/fish/vendor_completions.d/prayer-times.fish"
             '';
 
             meta = with pkgs.lib; {
-              description = "Islamic Prayer Times Information and Notifications";
-              homepage = "https://github.com/Yasso9/prayer-times";
+              description = cargoToml.package.description;
+              homepage = cargoToml.package.homepage;
               license = licenses.mit;
               mainProgram = "prayer-times";
+              platforms = supportedSystems;
             };
           };
+        in
+        {
+          inherit prayer-times;
+          default = prayer-times;
+        }
+      );
 
-          default = self.packages.${system}.prayer-times;
-        };
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            inputsFrom = [ self.packages.${system}.prayer-times ];
+            packages = with pkgs; [
+              rust-analyzer
+              clippy
+              rustfmt
+            ];
+          };
+        }
+      );
 
-        devShells.default = pkgs.mkShell {
-          inputsFrom = [ self.packages.${system}.prayer-times ];
-
-          packages = with pkgs; [
-            rust-analyzer
-            clippy
-            rustfmt
-          ];
-        };
-      }
-    )
-    // {
       overlays.default = _final: prev: {
         prayer-times = self.packages.${prev.system}.prayer-times;
       };
